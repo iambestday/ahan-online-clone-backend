e900356c1676d3cdb5a2caa1f0cf147d6b3ffb76
iambestday <behrouz.asghari@gmail.com>
2022-09-08 01:27:36 +0430

    🔥

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ require('dotenv').config();
 const express =require('express');
 const cors =require ("cors")
 const app = express ();
-
-
+const connection = require ("./db");
 
 
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(require("./routes/Routes"));
 
 const port= process.env.PORT;
 app.listen(port,()=> console.log(`Listening on port ${port}`));
 
-app.get ("/",(res,req)=>{
-    res.send("Back-END Run!");
-    });
+//database connection
+connection();
