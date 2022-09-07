882d45ba1cbfd6ea2f3f9eb21cc3955d40e46bf7
iambestday <behrouz.asghari@gmail.com>
2022-09-08 01:18:55 +0430

    🚑

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const cors =require ("cors")
 const app = express ();
 const connection = require ("./db");
 
-//database connection
-connection();
+
 
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(require("./routes/Routes"));
 
-const port= process.env.PORT || 5000;
+const port= process.env.PORT;
 app.listen(port,()=> console.log(`Listening on port ${port}`));
 
+app.get ("/",(res,req)=>{
+    res.send("Back-END Run!");
+    });
