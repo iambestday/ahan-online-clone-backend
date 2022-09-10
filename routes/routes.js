c682961aed5026e528225c0066e291cb7266ef57
iambestday <behrouz.asghari@gmail.com>
2022-09-10 22:45:14 +0430

    🚚 Merge routes

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ const express = require("express");
 const generateAuthToken = require("../utils/generateAuthToken");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/api/register", async (req, res) => {
   const schema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
     email: Joi.string().min(3).max(200).required().email(),
@@ -16,8 +16,7 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already exists...");
-  console.log("here");
-
+  
   const { name, email, password } = req.body;
   user = new User({ name, email, password });
   const salt = await bcrypt.genSalt(10);
@@ -27,4 +26,28 @@ router.post("/", async (req, res) => {
   res.send(token);
 });
 
-module.exports = router;
+router.post("/api/login", async (req, res) => {
+  const schema = Joi.object({
+    email: Joi.string().min(3).max(200).required().email(),
+    password: Joi.string().min(6).max(200).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password...");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res.status(400).send("Invalid email or password...");
+  const token = generateAuthToken(user);
+  res.send(token);
+});
+
+router.get("/api/userList" ,(req,res)=>{
+  User.find()
+    .then(findUsers => res.json(findUsers))
+});
+  
+
+  module.exports = router;
