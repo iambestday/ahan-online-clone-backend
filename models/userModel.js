b7614786debe79ba84121a128772ce6d7dc70e09
iambestday <behrouz.asghari@gmail.com>
2022-09-10 14:48:28 +0430

    🔧 Change userModel Config

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const userSchema = ({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  
-}
-)
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 30 },
+  email: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 200,
+    unique: true,
+  },
+  password: { type: String, required: true, minlength: 3, maxlength: 1024 },
+});
 
 const User = mongoose.model("User", userSchema);
-module.exports = User;
+exports.User = User;
