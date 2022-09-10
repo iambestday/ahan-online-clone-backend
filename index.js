393006d39b76c368b4a755b977023961b774ca6e
iambestday <behrouz.asghari@gmail.com>
2022-09-10 22:48:27 +0430

    🔧  update config

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const express = require('express');
 const cors = require("cors")
 const app = express();
 const connection = require("./db");
-
-const register = require("./routes/Register");
-const login = require("./routes/Login");
-const products = require("./routes/Products");
-
+const products = require ("./products")
+const routes = require("./routes/routes");
 
 //middleware
 app.use(express.json());
@@ -17,14 +14,12 @@ app.use(cors({origin: "*",}));
 
 //database connection
 connection();
-
-app.get("/", (req, res) => {res.send("Your API now is UP , Deployed successfully");});
-app.get("/api", (req, res) => {return res.status(200).send({ message: "Your API now is UP... =>", time: new Date() });});
-app.get("/api/products", (req, res) => {res.send(products);});
-
-app.use("/api/register", register);
-app.use("/api/login", login);
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+app.get("/", (req, res) => {res.send("Your API now is UP , Deployed successfully");});
+app.get("/api/products", (req, res) => {res.send(products);});
+app.use("/", routes);
+
 
